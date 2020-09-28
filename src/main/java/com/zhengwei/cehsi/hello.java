cd32c005b21d1473e4ce2c7729ec7445b2081c15
lexed <lexed@BD-02-52>
2020-09-28 16:08:43 +0800

    fenzhi tianjia

--- src/main/java/com/zhengwei/cehsi/hello.java
+++ src/main/java/com/zhengwei/cehsi/hello.java
@@ -4,6 +4,8 @@ public class hello {
 public static void main(String[] args) {
 	System.out.println("123213213");
 	System.out.println("123456");
+	System.out.println("zhugan");
+	System.out.println("fenzhi");
 }
 
 }
